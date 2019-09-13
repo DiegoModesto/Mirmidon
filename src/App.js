6272d6134cf8b505f9330c6dcf979539731b9b18
Diego Modesto <diego.modesto@avalara.com>
2019-09-13 16:23:30 -0300

    New files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Routes from './Routes'
+import validate from 'validate.js'
+import { Router } from 'react-router-dom'
+import { Chart } from 'react-chartjs-2';
+import { createBrowserHistory } from 'history'
+import { ThemeProvider } from '@material-ui/styles'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { chartjs } from './helpers'
+import theme from './theme'
+import validators from './common/validators';
+
+
+
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import './assets/scss/index.scss';
+
+const browserHistory = createBrowserHistory()
+
+Chart.helpers.extend(Chart.elements.Rectangle.prototype, {
+  draw: chartjs.draw
+})
+
+validate.validators = {
+  ...validate.validators,
+  ...validators
 }
 
-export default App;
+export default class App extends Component{
+  render(){
+    return(
+      <ThemeProvider theme={theme}>
+        <Router history={browserHistory}>
+          <Routes />
+        </Router>
+      </ThemeProvider>
+    )
+  }
+}
